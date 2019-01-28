96b4ef7f711367f7c23407c2d5c91482d9f4623a
Jonatan Mates <jonatan.mates1@gmail.com>
2019-01-28 19:16:46 +0100

    adding randomness to the enemies and simple death system

--- Assets/Scripts/SlimeController.cs
+++ Assets/Scripts/SlimeController.cs
@@ -1,29 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SlimeController : MonoBehaviour
 {
     public float moveSpeed;
 
-    public float timeBetweenMove;
-    private float timeBetweenMoveCounter;
-    public float timeToMove;
-    private float timeToMoveCounter;
+    public float timeBetweenMove; //time between moves
+    private float timeBetweenMoveCounter; 
+
+    public float timeToMove; //time it takes to move
+    private float timeToMoveCounter; 
 
     private Rigidbody2D myRigidbody;
+    private bool moving; //checks if the object is moving
+    private Vector3 moveDirection; //Determines direction to move
 
-    private bool moving;
-
-    private Vector3 moveDirection;
+    public float waitToReload; //Time to revival
+    private bool reloading;
+    private GameObject thePlayer;
 
     // Start is called before the first frame update
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
 
-        timeBetweenMoveCounter = timeBetweenMove;
-        timeToMoveCounter = timeToMove;
+        //timeBetweenMoveCounter = timeBetweenMove;
+        //timeToMoveCounter = timeToMove;
+
+        timeBetweenMoveCounter = Random.Range(timeBetweenMove * 0.75f, timeBetweenMove * 1.25f);
+        timeToMoveCounter = Random.Range(timeToMove * 0.75f, timeToMove * 1.25f);
 
     }
 
@@ -38,7 +45,8 @@ public class SlimeController : MonoBehaviour
             if (timeToMoveCounter < 0f)
             {
                 moving = false;
-                timeBetweenMoveCounter = timeBetweenMove;
+                //timeBetweenMoveCounter = timeBetweenMove;
+                timeBetweenMoveCounter = Random.Range(timeBetweenMove * 0.75f, timeBetweenMove * 1.25f);
             }
 
         }
@@ -50,11 +58,34 @@ public class SlimeController : MonoBehaviour
             if (timeBetweenMoveCounter < 0f)
             {
                 moving = true;
-                timeToMoveCounter = timeToMove;
+                //timeToMoveCounter = timeToMove;
+                timeToMoveCounter = Random.Range(timeToMove * 0.75f, timeToMove * 1.25f);
 
                 moveDirection = new Vector3(Random.Range(-1f, 1f) * moveSpeed, Random.Range(-1f, 1f) * moveSpeed, 0f);
 
             }
         }
+
+        if (reloading)
+        {
+            waitToReload -= Time.deltaTime;
+            if(waitToReload < 0f)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); //reloading the current level
+                thePlayer.SetActive(true);
+            }
+
+        }
     }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            other.gameObject.SetActive(false);
+            reloading = true;
+            thePlayer = other.gameObject;
+        }
+    } 
+
 }
